9a950e17d1ab3f12a6dd0f0c44f0397d46620334
floralindberg <floral@kth.se>
2025-01-23 09:45:04 +0100

    updated placeQueens

--- functions.c
+++ functions.c
@@ -1,179 +1,181 @@
 #include <stdio.h>
 
-//int board[8][8];
+// int board[8][8];
 int numberOfQueens = 0;
 
-//int *queenPlacement;
+// int *queenPlacement;
 
-//int *board = *queenPlacement;
+// int *board = *queenPlacement;
 
-int x=0;
-int y=0;
+int x = 0;
+int y = 0;
 
-
-
-
-int getQueenPosX(){
+int getQueenPosX()
+{
     return x;
 }
 
-int getQueenPosY(){
+int getQueenPosY()
+{
     return y;
 }
 
-void setQueenPosX(int v){
+void setQueenPosX(int v)
+{
     x = v;
 }
 
-void setQueenPosY(int v){
+void setQueenPosY(int v)
+{
     y = v;
 }
 
-void recursivePlacement(int *board){
+void recursivePlacement(int *board)
+{
 
-   // markUnavailableSpots;
-
+    // markUnavailableSpots;
 }
 
-void placeQueens(int board[8][8]){
+void placeQueens(int board[8][8])
+{
     int a = getQueenPosY();
     int b = getQueenPosX();
 
-    while(a < 8){
-
-        while (b < 8){
-            if (board[a][b] != 1 && board[a][b] != 2){
-                board[a][b] = 1;
-                numberOfQueens++;
-                y = a;
-                x = b;
-                printf("%s\n", "coord: ");
-                printf("%d", y);
-                printf("%d", x);
-                printf("%s\n", "end");
-                return;
-            }
-
-            if(board[a][b] == 1 || board[a][b] == 2){
-                b++;
-            }
-        }
+    if (a && b != 0)
+    {
         a++;
-        b = getQueenPosX();
+        b = 0;
     }
-        
+
+    while (b < 8)
+    {
+        if (board[a][b] != 1 && board[a][b] != 2)
+        {
+            board[a][b] = 1;
+            numberOfQueens++;
+            y = a;
+            x = b;
+            return;
+        }
+        b++;
+    }
 }
 
-int diagonal(int board[8][8], int m, int n, int mode){
+int diagonal(int board[8][8], int m, int n, int mode)
+{
     int posY = getQueenPosY();
     int posX = getQueenPosX();
 
-    while(posY >= 0 && posY <= 7 && posX >= 0 && posX <= 7){
-        posY = posY+m;
-        posX = posX+n;
-        if(posX || posY == (-1 || 8)){
-            return 1;
-        }
-        if(mode == 1){
-            board[posY][posX] = 2;
-        }else if (board[posY][posX] == 1){
-            //printf("%d\n", posY);
-            //printf("%d\n", posX);
-            return 0;
+    while (posY >= 0 && posY <= 7 && posX >= 0 && posX <= 7)
+    {
+        posY = posY + m;
+        posX = posX + n;
+        if (posY >= 0 && posY <= 7 && posX >= 0 && posX <= 7)
+        {
+            if (mode == 1)
+            {
+                board[posY][posX] = 2;
+            }
+            else if (board[posY][posX] == 1)
+            {
+                return 0;
+            }
         }
     }
     return 1;
 }
 
-int scanBoard(int board[8][8]){
+int scanBoard(int board[8][8])
+{
     int availableSlots = 0;
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            if(board[i][j] == 0){
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j] == 0)
+            {
                 availableSlots++;
             }
         }
     }
-    if (availableSlots >= (8 - numberOfQueens)){
+    if (availableSlots >= (8 - numberOfQueens))
+    {
         return 1;
-    }else return 0;
+    }
+    else
+        return 0;
 }
 
-
-
-/*x = (x+1)%8;
-    while(board[y][x] != 1){
-    }*/
-
-
-void markUnavailableSpots (int board[8][8]){
-    
-    
+void markUnavailableSpots(int board[8][8])
+{
 }
 
-void queenPosition(int *board){
-
+void queenPosition(int *board)
+{
 }
 
-int searchStraight(int board [8][8]) {
+int searchStraight(int board[8][8])
+{
     int a = getQueenPosY();
     int b = getQueenPosX();
 
     int temp = a + 1;
-    int temp2 = b +1;
+    int temp2 = b + 1;
 
-while (temp != a)
-{
-
-    if (board[temp][b] == 1)
+    while (temp != a)
     {
-        return 0;
+
+        if (board[temp][b] == 1)
+        {
+            return 0;
+        }
+
+        temp = (temp + 1) % 8;
     }
 
-    temp = (temp + 1) % 8;
-}
-
-while (temp2 != b)
-{
-
-    if (board[a][temp2] == 1)
+    while (temp2 != b)
     {
-        return 0;
+
+        if (board[a][temp2] == 1)
+        {
+            return 0;
+        }
+
+        temp2 = (temp2 + 1) % 8;
     }
 
-    temp2 = (temp2 + 1) % 8;
+    return 1;
 }
 
-return 1;
+void markStraight(int board[8][8])
+{
 
+    int a = getQueenPosY();
+    int b = getQueenPosX();
+
+    int temp = a + 1;
+    int temp2 = b + 1;
+
+    while (temp != a)
+    {
+        board[temp][b] = 2;
+        temp = (temp + 1) % 8;
+    }
+
+    while (temp2 != b)
+    {
+        board[a][temp2] = 2;
+        temp2 = (temp2 + 1) % 8;
+    }
 }
 
-void markStraight(int board[8][8]){
-    
-int a = getQueenPosY();
-int b = getQueenPosX();
+void mark(int board[8][8])
+{
 
-int temp = a + 1;
-int temp2 = b +1;
-
-while (temp != a) {
-    board[temp][b] = 2;
-    temp = (temp + 1) % 8;
-}
-
-while (temp2 != b) {
-    board[a][temp2] = 2;
-    temp2 = (temp2 + 1) % 8;
-}
-
-}
-
-void mark(int board[8][8]){
-
-    int a = diagonal(board,1,1,0);
-    int b = diagonal(board,-1,1,0);
-    int c = diagonal(board,1,-1,0);
-    int d = diagonal(board,-1,-1,0);
+    int a = diagonal(board, 1, 1, 0);
+    int b = diagonal(board, -1, 1, 0);
+    int c = diagonal(board, 1, -1, 0);
+    int d = diagonal(board, -1, -1, 0);
     int e = searchStraight(board);
 
     printf("%d\n", a);
@@ -185,43 +187,44 @@ void mark(int board[8][8]){
     printf("%d\n", y);
     printf("%d\n", x);
     printf("%s\n", "end");
-    
 
+    if (diagonal(board, 1, 1, 0) == 1 &&
+        diagonal(board, -1, 1, 0) == 1 &&
+        diagonal(board, 1, -1, 0) == 1 &&
+        diagonal(board, -1, -1, 0) == 1 &&
+        searchStraight(board) == 1)
+    {
 
-
-    if(diagonal(board,1,1,0) == 1 &&
-        diagonal(board,-1,1,0) == 1 &&
-        diagonal(board,1,-1,0) == 1 &&
-        diagonal(board,-1,-1,0) == 1 &&
-        searchStraight(board) == 1) {
-            
-        diagonal(board,1,1,1);
-        diagonal(board,-1,1,1);
-        diagonal(board,1,-1,1);
-        diagonal(board,-1,-1,1);
+        diagonal(board, 1, 1, 1);
+        diagonal(board, -1, 1, 1);
+        diagonal(board, 1, -1, 1);
+        diagonal(board, -1, -1, 1);
         markStraight(board);
-        }
-        else {
-            board[y][x] = 5;
-            numberOfQueens--;
-        }
+    }
+    else
+    {
+        board[y][x] = 2;
+        numberOfQueens--;
+    }
 }
 
-void recursive(int board[8][8]){
-    
+void recursive(int board[8][8])
+{
+
     placeQueens(board);
-    
-    if(scanBoard(board) == 1){
+
+    if (scanBoard(board) == 1)
+    {
         mark(board);
-    }else {
+    }
+    else
+    {
         printf("%s\n", "ended");
         return;
     }
 
     recursive(board);
-
 }
-    
 
 int main()
 {
@@ -231,59 +234,11 @@ int main()
     int i = 0;
     int j = 0;
 
-    //recursive(board);
+    // recursive(board);
 
     placeQueens(board);
     mark(board);
-
-    for (j = 0; j < 8; j++)
-    {
-        for (i = 0; i < 8; i++)
-        {
-            printf("%d", board[j][i]);
-            printf("%s", " ");
-        }
-        printf("%s", "\n");
-    }
     placeQueens(board);
-    mark(board);
-    
-    for (j = 0; j < 8; j++)
-    {
-        for (i = 0; i < 8; i++)
-        {
-            printf("%d", board[j][i]);
-            printf("%s", " ");
-        }
-        printf("%s", "\n");
-    }
-    placeQueens(board);
-    mark(board);
-    for (j = 0; j < 8; j++)
-    {
-        for (i = 0; i < 8; i++)
-        {
-            printf("%d", board[j][i]);
-            printf("%s", " ");
-        }
-        printf("%s", "\n");
-    }
-    placeQueens(board);
-    mark(board);
-    for (j = 0; j < 8; j++)
-    {
-        for (i = 0; i < 8; i++)
-        {
-            printf("%d", board[j][i]);
-            printf("%s", " ");
-        }
-        printf("%s", "\n");
-    }
-    placeQueens(board);
-    mark(board);
-    //markUnavailableSpots(board);
-    //queenPosition(*board);
-    //queenPosition(*board);
 
     for (j = 0; j < 8; j++)
     {

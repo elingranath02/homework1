8832937a182640fab36cb6b8bb5e80bac01af9a6
floralindberg <floral@kth.se>
2025-02-18 15:47:16 +0100

    first draft

---
+++ sharedbathroom/SharedBathroom.java
@@ -0,0 +1,118 @@
+package sharedbathroom;
+
+public class SharedBathroom {
+
+    private boolean womenTurn;
+    private boolean menTurn;
+    private int nrOfWomenInBathroom;
+    private int nrOfMenInBathroom;
+
+    public SharedBathroom() {
+        this.womenTurn = false;
+        this.menTurn = false;
+        this.nrOfWomenInBathroom = 0;
+        this.nrOfMenInBathroom = 0;
+    }
+
+    private void setWomenTurn(boolean value) {
+        this.womenTurn = value;
+    }
+
+    private void setMenTurn(boolean value) {
+        this.menTurn = value;
+    }
+
+    private void increaseWomen() {
+        this.nrOfWomenInBathroom++;
+    }
+
+    private void increaseMen() {
+        this.nrOfMenInBathroom++;
+    }
+
+    private void decreaseWomen() {
+        this.nrOfWomenInBathroom--;
+
+    }
+
+    private void decreaseMen() {
+        this.nrOfMenInBathroom--;
+    }
+
+    private boolean getWomenTurn(){
+        return this.womenTurn;
+        
+    }
+
+    private boolean getMenTurn(){
+        return this.menTurn;
+    }
+
+    private int getNrOfWomenInBathroom(){
+        return this.nrOfWomenInBathroom;
+    }
+
+    private int getNrOfMenInBathroom(){
+        return this.nrOfMenInBathroom;
+    }
+
+    public synchronized void womanEnter() {
+        // womenTurn = true;
+        setWomenTurn(true);
+
+        System.out.println("Value of lock: " + womenTurn);
+        System.out.println("Nr of women in bathr: " + getNrOfWomenInBathroom());
+        if (getMenTurn() == true) {
+            System.out.println("bajs");
+        }
+        while (getMenTurn()) {
+            System.out.println("Woman: " + Thread.currentThread().threadId() + " waiting");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        increaseWomen();
+        System.out.println("Woman: " + Thread.currentThread().threadId() + " enters bathroom");
+
+    }
+
+    public synchronized void manEnter() {
+        setMenTurn(true);
+
+        System.out.println("Value of lock: " + menTurn);
+        System.out.println("Nr of women in bathr: " + getNrOfWomenInBathroom());
+        if (getMenTurn() == true) {
+            System.out.println("bajs");
+        }
+        while (getMenTurn()) {
+            System.out.println("Man: " + Thread.currentThread().threadId() + " waiting");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        increaseMen();
+        System.out.println("Man: " + Thread.currentThread().threadId() + " enters bathroom");
+    }
+
+    public synchronized void womenExit() {
+        decreaseWomen();
+        System.out.println("Woman: " + Thread.currentThread().threadId() + " exits bathroom");
+
+        if (getNrOfWomenInBathroom() == 0) {
+            setWomenTurn(false);
+            notifyAll();
+        }
+
+    }
+
+    public synchronized void manExit() {
+        decreaseMen();
+        System.out.println("Man: " + Thread.currentThread().threadId() + " exits bathroom");
+        if (getNrOfMenInBathroom() == 0) {
+            setMenTurn(false);
+         }
+    }
+
+}
--- threads.java
+++ threads.java
@@ -1,11 +1,22 @@
-import java.lang.Object;
-import java.lang.Thread;
+import java.util.Random;
+
+import sharedbathroom.SharedBathroom;
 
 public class Threads extends Thread {
+
+    long maxSleep = 5000;
+    long maxSleepBathroom = 3000;
+
+    SharedBathroom bathroom = new SharedBathroom();
+
+    public Threads() {
+
+    }
+
     public static void main(String[] args) {
 
-        int maximun_threads = 5;
-        for (int i = 0; i < maximun_threads; i++) {
+        int maximum_threads = 6;
+        for (int i = 0; i < maximum_threads; i++) {
             Threads thread = new Threads();
             thread.start();
         }
@@ -13,8 +24,49 @@ public class Threads extends Thread {
         // System.out.println("This code is outside of the thread");
     }
 
+    public void threadSleep(long sleepTime) {
+
+        Random random = new Random();
+        long sleep = 0;
+        while (sleep == 0) {
+            sleep = random.nextLong(sleepTime);
+        }
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void run() {
-        System.out.println("Current thread name: " + Thread.currentThread().getName());
-        System.out.println("Current thread id: " + Thread.currentThread().threadId());
+        while (true) {
+
+            threadSleep(maxSleep);
+
+            if ((Thread.currentThread().threadId()) % 2 == 0) {
+                womanQueue();
+            } else {
+                manQueue();
+            }
+        }
+    }
+    // System.out.println("Current thread id: " +
+    // Thread.currentThread().threadId());
+
+    public void womanQueue() {
+
+        System.out.println("Woman: " + Thread.currentThread().threadId() + " in queue");
+        bathroom.womanEnter();
+        threadSleep(maxSleepBathroom);
+        bathroom.womenExit();
+
+    }
+
+    public void manQueue() {
+        System.out.println("Man: " + Thread.currentThread().threadId() + " in queue");
+        bathroom.manEnter();
+        threadSleep(maxSleepBathroom);
+        bathroom.manExit();
+
     }
 }
